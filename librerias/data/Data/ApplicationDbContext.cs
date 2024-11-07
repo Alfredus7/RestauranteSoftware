947f7bb084356c82ad2526aac7e7beee13b17f09
ASUS <ASUS@LAPTOP-NLA6AOOB>
2024-11-07 15:24:41 -0400

    Ya estan los roles Asignados

--- librerias/data/Data/ApplicationDbContext.cs
+++ librerias/data/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Data.Data.Entitys;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,108 @@ namespace RestauranteSoftware.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+            // Identificadores de roles
+            string ADMIN_ID = "5d4725d6-6dc4-4d3f-ab81-dda36159300e";
+            string ROLE_ID = "341743f0-asd2–42de-afbf-59kmkkmk72cf6";
+            string CAJERO_ROL_ID = "a7e02d5b-2a1c-4d42-bb91-8c971e983bb7";
+            string COCINERO_ROL_ID = "b8d54e89-4b8c-47b2-a3bf-6c0a9fa6c9b3";
+
+            // Identificadores de usuarios
+            string CAJERO_USER_ID = "7c7b1288-2a9d-4d5f-bf4a-2d8b7b3cbadd";
+            string COCINERO_USER_ID = "6d6c3c9f-1a7d-4d6a-af9a-4d8d3f2a6cdf";
+
+            // Seed del rol Admin
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                Id = ROLE_ID,
+                ConcurrencyStamp = ROLE_ID
+            });
+
+            // Seed del rol Cajero
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "Cajero",
+                NormalizedName = "CAJERO",
+                Id = CAJERO_ROL_ID,
+                ConcurrencyStamp = CAJERO_ROL_ID
+            });
+
+            // Seed del rol Cocinero
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "Cocinero",
+                NormalizedName = "COCINERO",
+                Id = COCINERO_ROL_ID,
+                ConcurrencyStamp = COCINERO_ROL_ID
+            });
+
+            // Crear usuario Admin y asignar contraseña
+            var adminUser = new IdentityUser
+            {
+                Id = ADMIN_ID,
+                Email = "admin@email.com",
+                EmailConfirmed = true,
+                UserName = "admin@email.com",
+                NormalizedUserName = "ADMIN@EMAIL.COM",
+            };
+
+            PasswordHasher<IdentityUser> ph = new PasswordHasher<IdentityUser>();
+            adminUser.PasswordHash = ph.HashPassword(adminUser, "As12345!");
+            builder.Entity<IdentityUser>().HasData(adminUser);
+
+            // Crear usuario Cajero y asignar contraseña
+            var cajeroUser = new IdentityUser
+            {
+                Id = CAJERO_USER_ID,
+                Email = "cajero@email.com",
+                EmailConfirmed = true,
+                UserName = "cajero@email.com",
+                NormalizedUserName = "CAJERO@EMAIL.COM",
+            };
+            cajeroUser.PasswordHash = ph.HashPassword(cajeroUser, "Caja7589$");
+            builder.Entity<IdentityUser>().HasData(cajeroUser);
+
+            // Crear usuario Cocinero y asignar contraseña
+            var cocineroUser = new IdentityUser
+            {
+                Id = COCINERO_USER_ID,
+                Email = "cocinero@email.com",
+                EmailConfirmed = true,
+                UserName = "cocinero@email.com",
+                NormalizedUserName = "COCINERO@EMAIL.COM",
+            };
+            cocineroUser.PasswordHash = ph.HashPassword(cocineroUser, "Coci6756#");
+            builder.Entity<IdentityUser>().HasData(cocineroUser);
+
+            // Asignar el rol Admin al usuario Admin
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ROLE_ID,
+                UserId = ADMIN_ID
+            });
+
+            // Asignar el rol Cajero al usuario Cajero
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = CAJERO_ROL_ID,
+                UserId = CAJERO_USER_ID
+            });
+
+            // Asignar el rol Cocinero al usuario Cocinero
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = COCINERO_ROL_ID,
+                UserId = COCINERO_USER_ID
+            });
+
         }
 
         public DbSet<ComidasEntitys> Comidas { get; set; }
---
+++ librerias/data/Data/Migrations/20241107191227_RolesCjCcn.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RestauranteSoftware.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesCjCcn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "Admin", "ADMIN" },
+                    { "a7e02d5b-2a1c-4d42-bb91-8c971e983bb7", "a7e02d5b-2a1c-4d42-bb91-8c971e983bb7", "Cajero", "CAJERO" },
+                    { "b8d54e89-4b8c-47b2-a3bf-6c0a9fa6c9b3", "b8d54e89-4b8c-47b2-a3bf-6c0a9fa6c9b3", "Cocinero", "COCINERO" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "5d4725d6-6dc4-4d3f-ab81-dda36159300e", 0, "be4aa689-f664-4c99-a927-bcfdb4d1e836", "admin@email.com", true, false, null, null, "ADMIN@EMAIL.COM", "AQAAAAIAAYagAAAAEPKPenRS7M3fsWBaN4yba6c3jq3OH3UKzIX6DOJeRYf0FakeI/71AtSVnpdG9z+dpA==", null, false, "80b6c890-88cf-400b-9013-28f3b4d0ab1c", false, "admin@email.com" },
+                    { "6d6c3c9f-1a7d-4d6a-af9a-4d8d3f2a6cdf", 0, "e770a4e5-0c7e-4b77-baa6-277a22e1656a", "cocinero@email.com", true, false, null, null, "COCINERO@EMAIL.COM", "AQAAAAIAAYagAAAAEOcymx5Y5ot8j0+6aX8AQnoPRkyW4/ejXmiA6VSgLrxyZuDfMdVharavbzvKuj8h7g==", null, false, "05777cf0-d4a3-49b8-88b2-539f2d7e9d08", false, "cocinero@email.com" },
+                    { "7c7b1288-2a9d-4d5f-bf4a-2d8b7b3cbadd", 0, "14496c64-eb2a-41cb-8a83-190f510a5326", "cajero@email.com", true, false, null, null, "CAJERO@EMAIL.COM", "AQAAAAIAAYagAAAAEOyUFD2/7arpLTs1sPIJXiAdYNRAzjRgP7SJ+LYcj6ogyt7tUmpvF49vH2RVc1Ck2Q==", null, false, "306f0ceb-dbef-4cbf-bae6-1cc9fb637d51", false, "cajero@email.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "5d4725d6-6dc4-4d3f-ab81-dda36159300e" },
+                    { "b8d54e89-4b8c-47b2-a3bf-6c0a9fa6c9b3", "6d6c3c9f-1a7d-4d6a-af9a-4d8d3f2a6cdf" },
+                    { "a7e02d5b-2a1c-4d42-bb91-8c971e983bb7", "7c7b1288-2a9d-4d5f-bf4a-2d8b7b3cbadd" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "5d4725d6-6dc4-4d3f-ab81-dda36159300e" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "b8d54e89-4b8c-47b2-a3bf-6c0a9fa6c9b3", "6d6c3c9f-1a7d-4d6a-af9a-4d8d3f2a6cdf" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "a7e02d5b-2a1c-4d42-bb91-8c971e983bb7", "7c7b1288-2a9d-4d5f-bf4a-2d8b7b3cbadd" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "341743f0-asd2–42de-afbf-59kmkkmk72cf6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a7e02d5b-2a1c-4d42-bb91-8c971e983bb7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b8d54e89-4b8c-47b2-a3bf-6c0a9fa6c9b3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "5d4725d6-6dc4-4d3f-ab81-dda36159300e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6d6c3c9f-1a7d-4d6a-af9a-4d8d3f2a6cdf");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "7c7b1288-2a9d-4d5f-bf4a-2d8b7b3cbadd");
+        }
+    }
+}
